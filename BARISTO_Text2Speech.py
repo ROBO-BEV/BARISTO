36def4e468941c0205adeebda582ba191d5b1c73
Blaze Sanders <blaze@infinifill.com>
2018-03-10 15:38:57 -0800

    Fixed switch statements

--- BARISTO_Text2Speech.py
+++ BARISTO_Text2Speech.py
@@ -14,37 +14,36 @@ __doc__ =     "Control PowerPoint presentation programmaticaly with keyboard"
 #
 #  @return NOTHING
 def awsPollyInterface(audioClipNum):
-	if GPIO.input(POLLY_LISTEN_PIN) == HIGH:
+	#if GPIO.input(POLLY_LISTEN_PIN) == HIGH:
 	#print("STARTING WORMY PYTHON GAME. HAVE FUN :)")
 	#from subprocess import check_call
 	#check_call("python ~/python_games/wormy.py",shell=True)
 
-	switch (audioClipNum):
-		case 1:
-			if DEBUG_STATEMENTS_ON: print("Hello, Is this your first time with us?")
+	if (audioClipNum == HELLO_AUDIO):
+		if DEBUG_STATEMENTS_ON:
+			print("Hello, Is this your first time with us?")
 			#AWS.Polly("Hello, Is this your first time with us?")
-		case 2:
-			if DEBUG_STATEMENTS_ON: 
-				print("Place your smart cup on the black pad.")
-				print("OR")
-				print("Tell me the last four digits of your ohone number.")
-		case 3:
-			if DEBUG_STATEMENTS_ON: print("Good morning, Rosie.")
-		case 4:
-			if DEBUG_STATEMENTS_ON: 
-				print("Large caffe mocha with sugar coming up")
-				range(timer 13 to 1)
-						keyboard.type(timer)
-								keyboard.type("...")
-		case 5:
-			if DEBUG_STATEMENTS_ON: print("Your coffee is ready, Have a great day!")
-		case 6:
-			if DEBUG_STATEMENTS_ON: print("You said your number was 555-555-5555 correct?")
-		case 7:
-			if DEBUG_STATEMENTS_ON: 
-				print("Your phone number is not registered with BARISTO.")
-				print("Please say your full phone number again.")
-		case 8:
-			if DEBUG_STATEMENTS_ON: 
-				print("I still can not find your phone number.")
-				print("Please scan the QR code below to download BARISTO from the Google Play store and register with us.")
+	if (audioClipNum == PLACE_AUDIO):
+		if DEBUG_STATEMENTS_ON:
+			print("Place your smart cup on the black pad.")
+			print("OR")
+			print("Tell me the last four digits of your ohone number.")
+	if (audioClipNum == GOOD_AUDIO):
+		if DEBUG_STATEMENTS_ON:
+			print("Good morning, Rosie.")
+	if (audioClipNum == COFFEE_AUDIO):
+		if DEBUG_STATEMENTS_ON:
+			print("Large caffe mocha with sugar coming up")
+			for timer in range(10):
+				keyboard.type(abs(10-timer))
+				keyboard.type("...")
+	if (audioClipNum == READY_AUDIO):
+		if DEBUG_STATEMENTS_ON:
+			print("Your coffee is ready, Have a great day!")
+	if (audioClipNum == NUMBER_AUDIO):
+		if DEBUG_STATEMENTS_ON:
+			print("You said your number was 555-555-5555 correct?")
+	if (audioClipNum == NOT_REGISTERED_AUDIO):
+		if DEBUG_STATEMENTS_ON: 
+			print("Your phone number is not registered with BARISTO.")
+			print("Please scan the QR code below to download BARISTO from the Google Play store and register with us.")
--- BARISTO_UX_Demo.py
+++ BARISTO_UX_Demo.py
@@ -4,7 +4,7 @@ __author__ =  "Blaze Sanders"
 __email__ =   "founders@robobev.com"
 __company__ = "Robotic Beverage Technologies Inc"
 __status__ =  "Development"
-__date__ =    "Late Updated: 2018-03-09"
+__date__ =    "Late Updated: 2018-03-10"
 __doc__ =     "Control PowerPoint presentation programmaticaly with keyboard"
 
 # @link https://docs.python.org/3/library/subprocess.html#replacing-os-popen-os-popen2-os-popen3
@@ -25,10 +25,11 @@ import pynput.keyboard
 from pynput.keyboard import Key, Controller
 
 # Allow BASH command to be run  inside  Python 2.7 code like this file
-import subprocess               
+import subprocess
 from  subprocess import Popen, PIPE
 
-#import BARISTO_Text2Speech
+# Allow demo to talk
+import BARISTO_Text2Speech
 
 PRODUCT_MODE = "PRODUCT"            # Final product configuration
 FIELD_MODE  = "FIELD"				# Non-Techanical repair person configuration
@@ -73,17 +74,17 @@ USER_DELAY_TIME  = 0.7				#Delay keyboard presses by 700 ms
 CPU_LOAD_DELAY = 0.02				#Delay 20 ms to reduce CPU load from reaching 100% during looping
 
 #Random constant for EMIC2 hardware and Amazon Web Service (AWS) Polly API
-EMIC2 = 222							
-POLLY = 333						
+EMIC2 = 222
+POLLY = 333
 
 #Pre-recorded .mp3 audio file
-HELLO_AUDIO = 1						#"Hello, Is this your first time with us?"		
+HELLO_AUDIO = 1						#"Hello, Is this your first time with us?"
 PLACE_AUDIO = 2						#TO-DO:
 GOOD_AUDIO = 3						#TO-DO:
-LARGE_AUDIO = 4						#TO-DO:
+COFFEE_AUDIO = 4						#TO-DO:
 READY_AUDIO = 5						#TO-DO:
 NUMBER_AUDIO = 6					#TO-DO:
-NOT_REGISTERED_AUDIO = 7			#TO-DO:
+NOT_REGISTERED_AUDIO = 7				#TO-DO:
 DOWNLOAD_AUDIO = 8					#TO-DO: DELETE?
 
 # Create a command line parser
@@ -95,7 +96,7 @@ parser.add_argument("-u", "--unit", type=str, default= FIELD_MODE, choices=[TEST
 parser.add_argument("-t", "--trace", type=int, default=0, help="Program trace level.")
 parser.add_argument("-f", "--filename", type=str, default="sampleData.txt", help="File to be transmitted.") #cam0.0.jpeg
 parser.add_argument("-l", "--loop", type=int, default=0, help="Set to 1 to loop file.") 
-args = parser.parse_args()~
+args = parser.parse_args()
 
 ##
 #  @brief Transition PowerPoint to the next slide using the right arrow key
@@ -128,11 +129,10 @@ def transitionToPreviousSlide():
 #
 #  @return NOTHING
 def text2Speech(interfaceName, audioClipNum):
-	switch(interfaceName):
-		case EMIC2:
-			emic2Interface(audioClipNum)	
-		case POLLY:
-			BARISTO_Text2Speech.awsPollyInterface(audioClipNum)	
+	if (interfacename == EMIC2):
+		BARISTO_Text2Speech.emic2Interface(audioClipNum)
+	if (interfaceName == POLLY):
+		BARISTO_Text2Speech.awsPollyInterface(audioClipNum)
 
 ##
 #  @brief Calls into V0.1 hardware and terminal demo
@@ -142,39 +142,38 @@ def text2Speech(interfaceName, audioClipNum):
 #  @return NOTHING
 #def emic2Interface(serialPort, caption):
 def emic2Interface(serialPort, audioClipNum):
-	switch(interfaceName):
-		case HELLO_AUDIO:
-			if DEBUG_STATEMENTS_ON: print("Hello, Is this your first time with us?")
-	  			#TO-DO: How do I loop or select next sentence to say?
-	  			#data = np.array([0xFF, 0xA1, 0xB5])
-	  			range(8):
-	  		  		#GPIO.output(EMIC_SIN_PIN, data[i])
-	  		  		serialPort.write("Hello World")
-	  		  		serialPort.flush()
-	  				GPIO.output(EMIC_SPEAK_PIN, HIGH)
-		case 2:
-			if DEBUG_STATEMENTS_ON: 
-				print("Place your smart cup on the black pad.")
-				print("OR")
-				print("Tell me the last four digits of your ohone number.")
-		case 3:
-			if DEBUG_STATEMENTS_ON: print("Good morning, Rosie.")
-		case 4:
-			if DEBUG_STATEMENTS_ON: 
-				print("Large caffe mocha with sugar coming up")
-				range(timer 10 to 1)
-					keyboard.type(timer)
-							keyboard.type("...")
-		case 5:
-			if DEBUG_STATEMENTS_ON: print("Your coffee is ready, Have a great day!")
-		case 6:
-			if DEBUG_STATEMENTS_ON: print("You said your number ended in 5555 correct?")
-		case 7:
-			if DEBUG_STATEMENTS_ON: 
-				print("Your phone number is not registered with BARISTO.")
-				print("Please scan the QR code below to download BARISTO from the Google Play store and register with us.")
+	if (audioClipNum == HELLO_AUDIO):
+		if DEBUG_STATEMENTS_ON:
+			print("Hello, Is this your first time with us?")
+  			#data = np.array([0xFF, 0xA1, 0xB5])
+  			for  i in range (8): #0 to 7
+  		  		#GPIO.output(EMIC_SIN_PIN, data[i])
+  		  		serialPort.write("Hello World")
+  		  		serialPort.flush()
+  				GPIO.output(EMIC_SPEAK_PIN, HIGH)
+	if (audioClipNum == PLACE_AUDIO):
+		if DEBUG_STATEMENTS_ON:
+			print("Place your smart cup on the black pad.")
+			print("OR")
+			print("Tell me the last four digits of your ohone number.")
+	if (audioClipNum == GOOD_AUDIO):
+		if DEBUG_STATEMENTS_ON: print("Good morning, Rosie.")
+	if (audioClipNum == COFFEE_AUDIO):
+		if DEBUG_STATEMENTS_ON:
+			print("Large caffe mocha with sugar coming up")
+			for timer in range(10):
+				keyboard.type(abs(10-timer))
+				keyboard.type("...")
+	if (audioClipNum == READY_AUDIO):
+		if DEBUG_STATEMENTS_ON: print("Your coffee is ready, Have a great day!")
+	if (audioClipNum == NUMBER_AUDIO):
+		if DEBUG_STATEMENTS_ON: print("You said your number ended in 5555 correct?")
+	if (audioClipNum == NOT_REGISTERED_AUDIO):
+		if DEBUG_STATEMENTS_ON:
+			print("Your phone number is not registered with BARISTO.")
+			print("Please scan the QR code below to download BARISTO from the Google Play store and register with us.")
 ##
-#  @brief Configure GPIO pin on a Pi 3 using BCM pin mode 
+#  @brief Configure GPIO pin on a Pi 3 using BCM pin mode
 #
 #  @param NONE
 #
@@ -184,16 +183,13 @@ def setupGPIO():
 	GPIO.setup(RFID_READ_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	GPIO.setup(COFFEE_READY_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	GPIO.setup(POLLY_LISTEN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.setup(EMIC_SIN_PIN, GPIO.SERIAL, pull_up_down=GPIO.PUD_UP)
 	GPIO.setup(EMIC_SPEAK_PIN, GPIO.OUT)
-	GPIO.setup(EMIC_SOUT_PIN, GPIO.SERIAL)
 	#https://spellfoundry.com/2016/05/29/configuring-gpio-serial-port-raspbian-jessie-including-pi-3/
-	#GPIO.setup(EMIC_SOUT_PIN, GPIO.I2C)
 
-	#Falling edge active LOW pins with 20 ms software debounce 
-	GPIO.add_event_detect(RFID_READ_PIN, GPIO.FALLING, callback=my_callback, bouncetime=20)  
-	GPIO.add_event_detect(COFFEE_READY_PIN, GPIO.FALLING, callback=my_callback, bouncetime=20) 
-	GPIO.add_event_detect(POLLY_LISTEN_PIN, GPIO.FALLING, callback=my_callback, bouncetime=20) 
+	#Falling edge active LOW pins with 20 ms software debounce
+	GPIO.add_event_detect(RFID_READ_PIN, GPIO.FALLING, callback=my_callback, bouncetime=20)
+	GPIO.add_event_detect(COFFEE_READY_PIN, GPIO.FALLING, callback=my_callback, bouncetime=20)
+	GPIO.add_event_detect(POLLY_LISTEN_PIN, GPIO.FALLING, callback=my_callback, bouncetime=20)
 
 ##
 #  @brief Read the currect logic level (HIGH or LOW) on an button press.
@@ -229,35 +225,34 @@ if __name__ == "__main__":
 	keyboard = Controller()
 	keyboard.type('ROBO BEV BARISTO demo starting now...')
 
-	serialPort1 = configureSerialPort()
+	#TO_DO: serialPort1 = configureSerialPort()
 	setupGPIO()
-	
+
 	try:
 
 		while True:
-			
-			range(2): #Repeat "Hello, Is this your first time with us?"  twice
+			for i in range(2): #Repeat "Hello, Is this your first time with us?"  twice
 				text2Speech(EMIC2, HELLO_AUDIO)
 				text2Speech(EMIC2, PLACE_AUDIO)
 				time.sleep(1500)
 
 			while not GPIO.event_detected(RFID_READ_PIN):	#Loop until active LOW pin falls
-				time.sleep(CPU_LOAD_DELAY) 
+				time.sleep(CPU_LOAD_DELAY)
 				#if ( loopCounter > CPU_LOAD_DELAY * 1000) RFID was not read
-				if  (loopCounter > 10000): 		
-					text2Speech(EMIC2, NUMBER_AUDIO) 
+				if  (loopCounter > 10000):
+					text2Speech(EMIC2, NUMBER_AUDIO)
 					keyboard.type('Yes')
 					text2Speech(EMIC2, NOT_REGISTERED_AUDIO)
-			transitionToNextSlide()				
+			transitionToNextSlide()
 			text2Speech(EMIC2, GOOD_AUDIO)
 			text2Speech(EMIC2, LARGE_AUDIO)
 
 			while GPIO.input(COFFEE_READY_PIN) == HIGH:		#Loop until active LOW pin falls
-				time.sleep(CPU_LOAD_DELAY) 							 
+				time.sleep(CPU_LOAD_DELAY)
 			transitionToNextSlide()
 			text2Speech(EMIC2, READY_AUDIO)
 
-			time.sleep(CPU_LOAD_DELAY) 
+			time.sleep(CPU_LOAD_DELAY)
 
 	except KeyboardInterrupt:
 		print "\n**Keyboard Interrupt Detected**\n"
