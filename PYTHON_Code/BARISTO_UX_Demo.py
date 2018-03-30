38de4fdca383515beda34d3e3593aa65c006e0b1
Blaze Sanders <blaze@robobev.com>
2018-03-30 13:36:06 -0700

    Fixed merge error by unstashing index {@1}

--- PYTHON_Code/BARISTO_UX_Demo.py
+++ PYTHON_Code/BARISTO_UX_Demo.py
@@ -4,8 +4,8 @@ __author__ =  "Blaze Sanders"
 __email__ =   "blaze@robobev.com"
 __company__ = "Robotic Beverage Technologies Inc"
 __status__ =  "Development"
-__date__ =    "Late Updated: 2018-03-26"
-__doc__ =     "Control PowerPoint presentation programmaticaly with software keyboard presses"
+__date__ =    "Late Updated: 2018-03-30"
+__doc__ =     "Control PowerPoint presentation programmaticaly with keyboard"
 
 # @link https://docs.python.org/3/library/subprocess.html#replacing-os-popen-os-popen2-os-popen3
 # @link http://codeandlife.com/2012/07/29/arduino-and-raspberry-pi-serial-communication/
@@ -26,13 +26,14 @@ from pynput.keyboard import Key, Controller
 
 # Allow BASH command to be run  inside  Python 2.7 code like this file
 import subprocess
-from  subprocess import Popen, PIPE
+from subprocess import Popen, PIPE
+from subprocess import check_call
 
 # Allow demo to talk
-import BARISTO_Text2Speech
+import Text2Speech
 
 PRODUCT_MODE = "PRODUCT"        # Final product configuration
-FIELD_MODE  = "FIELD" 		# Non-Techanical repair person configuration
+FIELD_MODE  = "FIELD"		# Non-Techanical repair person configuration
 TESTING_MODE = "TEST"           # Internal developer configuration
 DEBUG_STATEMENTS_ON = True      # Toogle debug statements on and off for this python file
 
@@ -60,12 +61,14 @@ NUM_OUTPUT_PINS = 4                	#This software instance of Raspberry Pi can 
 NUM_INPUT_PINS = 4                 	#This software instance of Raspberry Pi can have up to four input pins
 
 #Hardware connected to the Raspberry Pi 3
+EMIC_SOUT_PIN = 14			#Raspberry Pi Connector J8 BCM 14 =  UART0 TX
+EMIC_SIN_PIN = 15		        #Raspberry Pi Connector J8 BCM 15 =  UART0 RX
 RFID_READ_PIN = 22            		#Raspberry Pi Connector J8 BCM 22 =  P15
 EMIC_SIN_PIN = 23			#Raspberry Pi Connector J8 BCM 23 = P16
 EMIC_SOUT_PIN = 24			#Raspberry Pi Connector J8 BCM 24 = P18
-COFFEE_READY_PIN = 24		   	#Raspberry Pi Connector J8 BCM 24 =  P17
-POLLY_LISTEN_PIN = 25		   	#Raspberry Pi Connector J8 BCM 25 =  P22
-EMIC_SPEAK_PIN = 27	   		#Raspberry Pi Connector J8 BCM 27 =  P12
+COFFEE_READY_PIN = 24		   	#Raspberry Pi Connector J8 BCM 24 =  P18
+#TO-DO: POLLY_LISTEN_PIN = 5		   	#Raspberry Pi Connector J8 BCM 5 =  P29
+EMIC_SPEAK_PIN = 25	   		#Raspberry Pi Connector J8 BCM 25 =  P22
 
 #UART pins in BCM mode are: 14, 15 /dev/ttyAMA0
 
@@ -76,16 +79,6 @@ CPU_LOAD_DELAY = 0.02			#Delay 20 ms to reduce CPU load from reaching 100% durin
 #Random constant for EMIC2 hardware and Amazon Web Service (AWS) Polly API
 EMIC2 = 222
 POLLY = 333
-
-#Pre-recorded .mp3 audio file
-HELLO_AUDIO = 1				#"Hello, Is this your first time with us?"
-PLACE_AUDIO = 2				#TO-DO:
-GOOD_AUDIO = 3				#TO-DO:
-COFFEE_AUDIO = 4			#TO-DO:
-READY_AUDIO = 5				#TO-DO:
-NUMBER_AUDIO = 6			#TO-DO:
-NOT_REGISTERED_AUDIO = 7		#TO-DO:
-DOWNLOAD_AUDIO = 8			#TO-DO: DELETE?
 
 # Create a command line parser
 parser = argparse.ArgumentParser(prog = "BARISTO V1", description = __doc__, add_help=True)
@@ -106,8 +99,8 @@ args = parser.parse_args()
 #  @return NOTHING
 def transitionToNextSlide():
 	# Press and release right arrow key
-	keyboard.press('Key.right')
-	keyboard.release('Key.right')
+	keyboard.press('Key.Right')
+	keyboard.release('Key.Right')
 
 ##
 #  @brief Transition PowerPoint to the previous slide using the left arrow key
@@ -117,8 +110,8 @@ def transitionToNextSlide():
 #  @return NOTHING
 def transitionToPreviousSlide():
 	# Press and release left arrow key
-	keyboard.press('Key.left')
-	keyboard.release('Key.left')
+	keyboard.press('Key.Left')
+	keyboard.release('Key.Left')
 
 ##
 #  @brief Generic text to speech function call
@@ -128,50 +121,11 @@ def transitionToPreviousSlide():
 #  @param audioClipNum Audio clip to play from speaker
 #
 #  @return NOTHING
-def text2Speech(interfaceName, audioClipNum):
-	if (interfacename == EMIC2):
-		BARISTO_Text2Speech.emic2Interface(audioClipNum)
+def say(interfaceName, audioClipNum):
+	if (interfaceName == EMIC2):
+		Text2Speech.emic2Interface(audioClipNum)
 	if (interfaceName == POLLY):
-		BARISTO_Text2Speech.awsPollyInterface(audioClipNum)
-
-##
-#  @brief Calls into V0.1 hardware and terminal demo
-#
-#  @param interfaceName Either EMIC 2 or AWS Polly
-#
-#  @return NOTHING
-#def emic2Interface(serialPort, caption):
-def emic2Interface(serialPort, audioClipNum):
-	if (audioClipNum == HELLO_AUDIO):
-		if DEBUG_STATEMENTS_ON:
-			print("Hello, Is this your first time with us?")
-  			#data = np.array([0xFF, 0xA1, 0xB5])
-  			for  i in range (8): #0 to 7
-  		  		#GPIO.output(EMIC_SIN_PIN, data[i])
-  		  		serialPort.write("Hello World")
-  		  		serialPort.flush()
-  				GPIO.output(EMIC_SPEAK_PIN, HIGH)
-	if (audioClipNum == PLACE_AUDIO):
-		if DEBUG_STATEMENTS_ON:
-			print("Place your smart cup on the black pad.")
-			print("OR")
-			print("Tell me the last four digits of your ohone number.")
-	if (audioClipNum == GOOD_AUDIO):
-		if DEBUG_STATEMENTS_ON: print("Good morning, Rosie.")
-	if (audioClipNum == COFFEE_AUDIO):
-		if DEBUG_STATEMENTS_ON:
-			print("Large caffe mocha with sugar coming up")
-			for timer in range(10):
-				keyboard.type(abs(10-timer))
-				keyboard.type("...")
-	if (audioClipNum == READY_AUDIO):
-		if DEBUG_STATEMENTS_ON: print("Your coffee is ready, Have a great day!")
-	if (audioClipNum == NUMBER_AUDIO):
-		if DEBUG_STATEMENTS_ON: print("You said your number ended in 5555 correct?")
-	if (audioClipNum == NOT_REGISTERED_AUDIO):
-		if DEBUG_STATEMENTS_ON:
-			print("Your phone number is not registered with BARISTO.")
-			print("Please scan the QR code below to download BARISTO from the Google Play store and register with us.")
+		Text2Speech.awsPollyInterface(audioClipNum)
 ##
 #  @brief Configure GPIO pin on a Pi 3 using BCM pin mode
 #
@@ -182,14 +136,37 @@ def setupGPIO():
 	GPIO.setmode(GPIO.BCM)  #e.g. Raspberry Pi Connector J8 BCM 22 =  P15
 	GPIO.setup(RFID_READ_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	GPIO.setup(COFFEE_READY_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.setup(POLLY_LISTEN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	#TO-DO: GPIO.setup(POLLY_LISTEN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	GPIO.setup(EMIC_SPEAK_PIN, GPIO.OUT)
 	#https://spellfoundry.com/2016/05/29/configuring-gpio-serial-port-raspbian-jessie-including-pi-3/
 
 	#Falling edge active LOW pins with 20 ms software debounce
-	GPIO.add_event_detect(RFID_READ_PIN, GPIO.FALLING, callback=my_callback, bouncetime=20)
-	GPIO.add_event_detect(COFFEE_READY_PIN, GPIO.FALLING, callback=my_callback, bouncetime=20)
-	GPIO.add_event_detect(POLLY_LISTEN_PIN, GPIO.FALLING, callback=my_callback, bouncetime=20)
+	GPIO.add_event_detect(RFID_READ_PIN, GPIO.FALLING, callback=userInputStateMachine, bouncetime=20)
+	GPIO.add_event_detect(COFFEE_READY_PIN, GPIO.FALLING, callback=userOutputStateMachine, bouncetime=20)
+	#TO-DO: GPIO.add_event_detect(POLLY_LISTEN_PIN, GPIO.FALLING, callback=userInputStateMachine, bouncetime=20)
+
+	if (DEBUG_STATEMENTS_ON):
+		GPIO.setwarnings(True)
+		print('')
+		print('')
+		print('')
+	else:
+		#TO-DO: THIS IS NOT SUPPRESSING WARNINGS!!!!
+		GPIO.setwarnings(False)
+		print('')
+		print('')
+		print('')
+
+def userInputStateMachine():
+	print('User INPUT State Machine running')
+
+
+
+def userOutputStateMachine():
+	print('User OUTPUT  State Machine running')
+
+
+
 
 ##
 #  @brief Read the currect logic level (HIGH or LOW) on an button press.
@@ -223,34 +200,47 @@ def displayAllInputPins():
 if __name__ == "__main__":
 
 	keyboard = Controller()
-	keyboard.type('ROBO BEV BARISTO demo starting now...')
 
 	#TO_DO: serialPort1 = configureSerialPort()
 	setupGPIO()
+	check_call("clear",shell=True) #Clear warnings from terminal
+
+	keyboard.type('ROBO BEV BARISTO demo starting now...')
+	print('')
+	print('')
 
 	try:
 
 		while True:
-			for i in range(2): #Repeat "Hello, Is this your first time with us?"  twice
-				text2Speech(EMIC2, HELLO_AUDIO)
-				text2Speech(EMIC2, PLACE_AUDIO)
-				time.sleep(1500)
+			for i in range(2):
+				say(EMIC2, Text2Speech.HELLO_AUDIO)
+				say(EMIC2, Text2Speech.PLACE_AUDIO)
+				time.sleep(5)
+				check_call("clear",shell=True) #Clear previous user promt from terminal
 
-			while not GPIO.event_detected(RFID_READ_PIN):	#Loop until active LOW pin falls
+ 			loopCounter = 0
+			waitingOnUser = True
+			while not (GPIO.event_detected(RFID_READ_PIN)):	#Loop until active LOW pin falls
 				time.sleep(CPU_LOAD_DELAY)
-				#if ( loopCounter > CPU_LOAD_DELAY * 1000) RFID was not read
-				if  (loopCounter > 10000):
-					text2Speech(EMIC2, NUMBER_AUDIO)
-					keyboard.type('Yes')
-					text2Speech(EMIC2, NOT_REGISTERED_AUDIO)
+				if (loopCounter > CPU_LOAD_DELAY * 1000): #RFID tag was not read
+					say(EMIC2, Text2Speech.NUMBER_AUDIO)
+					time.sleep(2)
+					keyboard.type('Yes, that is my cell phone number.')
+					print('')
+					print('')
+					time.sleep(2)
+					say(EMIC2, Text2Speech.NOT_REGISTERED_AUDIO)
+					break
+				loopCounter = loopCounter + 1
+
 			transitionToNextSlide()
-			text2Speech(EMIC2, GOOD_AUDIO)
-			text2Speech(EMIC2, LARGE_AUDIO)
+			say(EMIC2, Text2Speech.GOOD_AUDIO)
+			say(EMIC2, Text2Speech.LARGE_AUDIO)
 
 			while GPIO.input(COFFEE_READY_PIN) == HIGH:		#Loop until active LOW pin falls
 				time.sleep(CPU_LOAD_DELAY)
 			transitionToNextSlide()
-			text2Speech(EMIC2, READY_AUDIO)
+			say(EMIC2, Text2Speech.READY_AUDIO)
 
 			time.sleep(CPU_LOAD_DELAY)
 
